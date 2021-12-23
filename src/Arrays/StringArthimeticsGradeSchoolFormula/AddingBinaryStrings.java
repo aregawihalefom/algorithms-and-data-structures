426a870926b6047413a9c40855706920cd61f821
aregawihalefom <aregawihalefom90@gmail.com>
2021-12-23 10:38:49 -0600

    Basic School arithmetic operations using Strings and Array

---
+++ src/Arrays/StringArthimeticsGradeSchoolFormula/AddingBinaryStrings.java
@@ -0,0 +1,56 @@
+package Arrays.StringArthimeticsGradeSchoolFormula;
+
+/***
+ *
+ * Description : Given two binary strings a and b, return their sum as a binary string.
+ *
+ * Example : Input: a = "11", b = "1"
+ *          Output: "100"
+ *
+ *  Approach 1 : Using Basic school addition procedure
+ *              - Running Time : 0(n)
+ *              - Space : 0(1)
+ */
+
+public class AddingBinaryStrings {
+
+    public String addBinary(String a, String b) {
+
+        // Base Cases
+        if(a.isEmpty()) return b;
+        if(b.isEmpty()) return a;
+
+        // Result accumulator
+        StringBuilder sb = new StringBuilder();
+
+        int carry = 0 ;
+        int lenA = a.length()-1;
+        int lenB = b.length()-1;
+
+
+        while(lenA >=0 || lenB >=0) {
+
+            int valA = 0;
+            int valB = 0;
+
+            // Changing to integers
+            if(lenA >=0)  valA =  a.charAt(lenA--) == '1' ? 1 : 0;
+            if(lenB >=0)  valB =  b.charAt(lenB--) == '1' ? 1 : 0;
+
+
+            if ((valA & valB) == 1) { // Both 1
+                sb = (carry == 1) ? sb.append(1) : sb.append(0); // possibility  : 11 ( with carry) or 10( without carry)
+                carry = 1;
+            } else if ((valA | valB) == 0) { // both zeros
+                sb = carry == 1 ? sb.append(1) : sb.append(0); // 1 ( when there is carry)
+                carry = 0;
+            } else { // are different
+                sb = carry == 1 ? sb.append(0) : sb.append(1);
+            }
+
+        }
+        // check if there is carry still
+        if(carry == 1) sb.append(carry);
+        return sb.reverse().toString();
+    }
+}
---
+++ src/Arrays/StringArthimeticsGradeSchoolFormula/IncrementArrayByOne.java
@@ -0,0 +1,72 @@
+package Arrays.StringArthimeticsGradeSchoolFormula;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/***
+ *
+ *  Description : Write a program which takes as input an array of digits encoding a decimal number
+ *                D and updates the array to represent the number D + 1. For example, if the input
+ *                is (1,2,9) then you should update the array to (1,3,0). Your algorithm should work
+ *                even if it is implemented in a language that has finite-precision arithmetic.
+ *
+ *  Example : 1. Input : {1, 2, 9} => Output : {1, 3, 0}
+ *            2. Input : {9, 9, 9} => Output : {1, 0, 0, 0}
+ *
+ *
+ *  Application : Addition of large numbers that cannot be performed in the range of integers. Particularly , this
+ *                is a problem with languages the imposes limit over range of types.
+ *                We can avoid overflow issues by operating directly on the array of digits.
+ *
+ *
+ *  Naive  : move the array into an integer and then perform addition and then copy back the digits to the array
+ *          Problems : Overflow is likely
+ *          Running Time : 0(n) -> n is equal to the number of digits in the number or size of the input array
+ *          Space : 0(1)
+ *
+ *
+ * Best Approach : Use grade school addition using a carry variables
+ *                 Code : gradeSchoolAddition method
+ *
+ *
+ *  Variants : 1. Adding two Binary Strings Bt and Bs : Important point is following the addition
+ *             2. Adding two arrays
+ *             3. Adding Two linked lists
+ *             4. Multiply String
+ *             5. Adding Strings
+ */
+
+public class IncrementArrayByOne {
+
+    public static List<Integer> addOneToArray(List<Integer> list){
+
+        int n = list.size()-1;
+
+        // We only add 1 to the last element
+        list.set(n,  list.get(n) + 1);
+
+        for(int i = n ; i > 0 && list.get(i) >=10; i--){
+                list.set(i, 0);
+                list.set(i-1, list.get(i-1)+1); // move this to the next as a carry
+        }
+
+        if(list.get(0) == 10){
+            list.set(0, 0);
+            list.add(0, 1);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list1 = new ArrayList<Integer>() {{add(1);add(2);add(9);}};
+        System.out.println(addOneToArray(list1));
+
+        ArrayList<Integer> list2 = new ArrayList<Integer>() {{add(9);add(9);add(9);}};
+        System.out.println(addOneToArray(list2));
+    }
+
+
+}
